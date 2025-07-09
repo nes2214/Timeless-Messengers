4477d9d8c0dba960ae95959ea569706f51c130b5
nes2214 <cuevasnestor06@gmail.com>
2025-07-09 12:53:41 +0200

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import sitemap from 'astro-sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(),react(), ],
+  site: 'https://ncrdom.cat',  // Aquí pones la URL base de tu sitio
+  integrations: [mdx(), react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
